f0f619cfa95910f642f1f3b992f346805d767766
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-04-10 22:21:50 +0000

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 from payment.models import *
+from django.contrib.auth.decorators import login_required
+@login_required
 def home(request):
     invoice = Invoice.objects.filter(created_by=request.user).first()
     return render(request,"home.html",context={"invoice":invoice})
