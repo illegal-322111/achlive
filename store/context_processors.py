966494f0ddbbb1e25b8acec26d303924d61873bd
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-04-20 02:37:08 +0000

    Update context_processors.py

--- store/context_processors.py
+++ store/context_processors.py
@@ -14,8 +14,12 @@ def categories(request):
 def balance(request):
     if request.user.is_authenticated:
         balance = Balance.objects.filter(created_by=request.user).first()
-        b = round(balance.balance, 2)
-        return {'balance': b}
+        if balance:
+            b = round(balance.balance, 2)
+            return {'balance': b}
+        else:
+            b = 0.00
+            return {'balance': b}
     else:
         b = 0.00
         return {'balance': b}
