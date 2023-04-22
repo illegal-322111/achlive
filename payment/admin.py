ee5eea3483e65efd6c4975fea060db89f1d17f0c
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-04-22 21:16:27 +0000

    Update admin.py

--- payment/admin.py
+++ payment/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from .models import *
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('status', 'order_id', 'address', 'btcvalue', 'received', 'sold', 'created_by','created_at')
+    list_display = ('status','product', 'order_id', 'address', 'btcvalue', 'received', 'sold', 'created_by','created_at')
     list_filter = ('sold',"received")
     search_fields = ('created_by','order_id')
     
-    list_editable = ('sold','created_at')
+    list_editable = ('sold','created_at','product')
 
     fieldsets = (
         (None, {
-            'fields': ('status', 'order_id', 'address', 'btcvalue', 'received', 'balance', 'created_by','created_at')
+            'fields': ('status', 'order_id', 'address', 'btcvalue', 'received', 'product', 'created_by','created_at')
         }),
     )
 admin.site.register(Invoice, InvoiceAdmin)
