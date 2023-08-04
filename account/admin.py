4cd811807f5a2311a11d850ca7c68be7aac4511a
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-04 14:35:07 +0000

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -5,7 +5,7 @@ from .models import Customer
 @admin.register(Customer)
 class Admin(admin.ModelAdmin):
     list_display = ('email','user_name','is_active')
-    list_filter = ("is_active")
+    list_filter = ('is_active')
     search_fields = ('user_name','email')
     
     list_editable = ('is_active')
