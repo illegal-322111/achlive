4a0c7296674566f29f6bd5ae514a6db5fad24789
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-04 22:35:42 +0000

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Customer
 
-admin.site.register(Customer)
+#admin.site.register(Customer)
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    '''Admin View for Customer'''
+
+    list_display = ('user_name','email','is_active')
+    list_filter = ('is_active')
+    
+    readonly_fields = ('email')
+    search_fields = ('user_name')
+    
