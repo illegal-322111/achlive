58feb29427d1ccc6a1011991a9b20c470f685e19
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-08-04 14:51:51 +0000

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from .models import Customer
 
+admin.site.register(Customer)
 # Register your models here.
-@admin.register(Customer)
-class Admin(admin.ModelAdmin):
-    list_display = ('email','user_name','is_active')
-    list_filter = ('is_active')
-    search_fields = ('user_name','email')
-    
-    list_editable = ('is_active')
