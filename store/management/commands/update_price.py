b3acaaf95f0c893ba8b710017d2fe90519b36b85
DeAgusco <111620386+DeAgusco@users.noreply.github.com>
2023-10-29 15:33:04 +0000

    ok

--- store/management/commands/update_price.py
+++ store/management/commands/update_price.py
@@ -1,19 +1,17 @@
-import random
-from decimal import Decimal
 from django.core.management.base import BaseCommand
-from payment.models import Balance
+from store.models import Product
 
 class Command(BaseCommand):
     help = 'Randomly update balances and prices'
 
     def handle(self, *args, **options):
-        # Define pr
-        balances = Balance.objects.all()
-        for balance in balances:
-            if balance.balance is None:
-                balance.balance = 0
-                balance.save()
-            else:
-                pass
+        
+        # Filter products based on the Balance field
+        filtered_products = Product.objects.filter(price=350)
+
+        # Update the selected products with the new balance
+        for product in filtered_products:
+            product.Balance = str(float(product.Balance) * 10)
+            product.save()
 
         self.stdout.write(self.style.SUCCESS('Balances updated successfully.'))
